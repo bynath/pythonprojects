05adfd28970c582f8c8e91b2ae94ac54452048f7
Nathalia Assis <ct.nathaliaassis@hotmail.com>
2026-09-09 22:07:44 -0300

    Adicionar o cálculo da média das notas dos alunos

---
+++ media_escolar.py
@@ -0,0 +1,15 @@
+notas = []
+qtde = int(input("Para quantas notas você quer fazer a média? "))
+
+if qtde < 2:
+  print("Informe pelo menos duas notas para calcular a média")
+
+else:
+  for i in range(qtde):
+    nota = float(input("Informe a sua nota: "))
+    notas.append(nota)
+
+  print(notas)
+  media = sum(notas) / len(notas)
+
+  print(f"A média de {notas} é {media:.2f}")
